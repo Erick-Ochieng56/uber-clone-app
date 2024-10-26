6abf5600383f521bb528479536b1972ecaf120c2
Erick Ochieng <erickochieng830@gmail.com>
2024-10-26 16:54:14 +0300

    home screen UI

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View, SafeAreaView} from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Image} from 'react-native';
+import React from 'react';
+import tw from 'twrnc';
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>I am the HomeScreen</Text>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View>
+        <Image 
+          style={{
+          width: 100,
+          height: 100,
+          resizeMode: 'contain',
+        }}
+          source={{
+          uri: "https://links.papareact.com/gzs",
+        }}
+        />
+      </View>
     </SafeAreaView>
   )
 }
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  text: {
+    color:"blue",
+  },
+});
