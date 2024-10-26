c7af0afd05bf625409afaf72870a14534259f32b
Erick Ochieng <erickochieng830@gmail.com>
2024-10-26 15:33:26 +0300

    home screen UI

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,1 +1,14 @@
-rmfce
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>I am the HomeScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
