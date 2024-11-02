33f6bda0bf76ace8cc4fd0fdf2cb55fb53b95ee4
Erick Ochieng <erickochieng830@gmail.com>
2024-11-02 17:58:37 +0300

    home screen UI and Map screen UI initialization

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,12 +1,14 @@
-import { StyleSheet, Text, View, FlatList} from 'react-native'
+import { StyleSheet, Text, View, FlatList, Image} from 'react-native'
 import React from 'react'
+import tw from 'twrnc';
 import { TouchableOpacity } from 'react-native';
+import { Icon } from '@rneui/base';
 
 const data = [
     {
         id:"123",
         title: "Get a ride",
-        Image: "https://links.papareact.com/3pn",
+        image: "https://links.papareact.com/3pn",
         screen: "MapScreen",
     },
     {
@@ -25,12 +27,26 @@ const NavOptions = () => {
     <View>
       <FlatList
       data={data}
-
+      horizontal
       keyExtractor={(item) => item.id}
       
       renderItem={({ item }) =>(
-        <TouchableOpacity>
-            <Text>{item.title}</Text>
+        <TouchableOpacity 
+             style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+            <View>
+              <Image
+              style={{width: 120,
+                 height:120,
+                 resizeMode: "contain"}}
+              source={{uri: item.image}}/>
+              <Text style={tw`m-2 text-lg font-semibold`}>{item.title}</Text>
+              
+              <Icon 
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name="arrowright" 
+              color="white" 
+              type="antdesign"/>
+            </View>
         </TouchableOpacity>
 
       )}
